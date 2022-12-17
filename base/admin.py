ade0efb4637bfaf2778c3a6f21abdb6707dc09f4
pettrop <p.tropp@gmail.com>
2022-12-17 14:04:19 +0100

    Fix bugs 2

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,73 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 from base.models import Room, Message
 
 
+class MessageAdmin(ModelAdmin):
+    @staticmethod
+    def cleanup_body(modeladmin, request, queryset):
+        queryset.update(body="--- Deleted ---")
+
+    ordering = ['id']
+    list_display = ['id', 'body', 'room']
+    list_display_links = ['id']
+    list_per_page = 20
+    list_filter = ['room']
+    search_fields = ['body', 'id']
+    actions = ['cleanup_body', 'body_short']
+
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ['id', 'body']
+            }
+        ),
+        (
+            'Detail',
+            {
+                'fields': ['room', 'created', 'updated'],
+                'description': 'Detailed information about room'
+            }
+        ),
+        (
+            'User information',
+            {
+                'fields': ['user']
+            }
+        )
+
+    ]
+    readonly_fields = ['id', 'created', 'updated']
+
+
+class RoomAdmin(ModelAdmin):
+    ordering = ['name']
+    list_display = ['id', 'name', 'description']
+    list_display_links = ['id', 'name']
+    list_per_page = 20
+    search_fields = ['name', 'description']
+
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ['id', 'name', 'description']
+            }
+        ),
+        (
+            'Detail',
+            {
+                'fields': ['participants', 'created', 'updated'],
+                'description': 'Detailed information about room'
+            }
+        ),
+
+    ]
+    readonly_fields = ['id', 'created', 'updated']
+
+
 # Register your models here.
-admin.site.register(Room)
-admin.site.register(Message)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Message, MessageAdmin)
--- base/models.py
+++ base/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import Model
 from django.contrib.auth.models import User
+from django.template.defaultfilters import truncatechars
 
 
 # Create your models here.
@@ -24,6 +25,9 @@ class Message(Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def body_short(self):
+        return truncatechars(self.body, 50)
 
     def __str__(self):
         return self.body[0:50]
